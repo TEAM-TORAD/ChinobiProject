70e4f6047f293b73ababd330bdc75b6cad0ad223
Olof <1017258@student.sae.edu.au>
2020-10-15 16:57:20 +1100

    Gold coin prefab and pickup functionality added
    Gold panel added in UI. Gold coin with particle effects added. Gold amount glows when picking up gold. Script called Economy added - use 1 in each scene!

---
+++ Chinobi/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    public int goldValue = 10;
+    Hashtable hash;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Rotate();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.transform.CompareTag("Player")) {
+            Economy.economy.AddGold(goldValue);
+            Destroy(transform.parent.gameObject);
+        }
+    }
+    void Rotate()
+    {
+        // http://www.pixelplacement.com/itween/documentation.php#RotateAdd
+        iTween.RotateAdd(transform, transform.gameObject , iTween.Hash(
+            "space", Space.World,
+            "time", 2.0f,
+            "amount", new Vector3(0.0f,360.0f,0.0f),
+            "easeType", iTween.EaseType.spring,
+            "loopType", iTween.LoopType.none,
+            "oncomplete", "RotationComplete",
+            "oncompletetarget", transform.gameObject
+            ));
+
+    }
+    public void RotationComplete()
+    {
+        print("Rotation is completed.");
+        Rotate();
+    }
+}
---
+++ Chinobi/Assets/Scripts/Economy.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Economy : MonoBehaviour
+{
+    public static Economy economy;
+    public GameObject inventoryGoldPrefab;
+    public int gold;
+    Transform goldPanel;
+    TextMeshProUGUI goldText;
+    public float glowTimePeak = 1.5f;
+    private bool updateGlow = false;
+    private float currentStrength, updateValue;
+    
+
+    private void Awake()
+    {
+        if (economy == null) economy = this;
+        else Destroy(this);
+        goldPanel = GameObject.FindGameObjectWithTag("GoldPanel").transform;
+        GameObject thisObject = Instantiate(inventoryGoldPrefab, goldPanel);
+        goldText = thisObject.transform.GetComponentInChildren<TextMeshProUGUI>();
+        goldText.text = gold.ToString();
+        SetGlowStrength(currentStrength);
+
+    }
+    private void Update()
+    {
+        if(updateGlow)
+        {
+            currentStrength += Time.deltaTime * updateValue;
+            SetGlowStrength(currentStrength);
+        }
+    }
+    private void SetGlowStrength(float value)
+    {
+        if (value < 0) value = 0;
+        else if (value > 1) value = 1.0f;
+        goldText.font.material.SetFloat(ShaderUtilities.ID_GlowPower, value);
+    }
+    public void AddGold(int amount)
+    {
+        gold += amount;
+        goldText.text = gold.ToString();
+        GlowText();
+    }
+    public void GlowText()
+    {
+        updateGlow = true;
+        updateValue = 1 / glowTimePeak;
+        StartCoroutine(GlowTextCR());
+    }
+    IEnumerator GlowTextCR()
+    {
+        
+        yield return new  WaitForSeconds(glowTimePeak);
+        updateValue = -1 / glowTimePeak;
+        yield return new WaitForSeconds(glowTimePeak);
+        updateGlow = false;
+        currentStrength = 0;
+        SetGlowStrength(currentStrength);
+    }
+}
