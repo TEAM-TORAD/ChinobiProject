6714351fda5f38ad95987a1daf598cdfd5004085
Tom <tomtomcoromandel@gmail.com>
2020-11-28 19:04:46 +1100

    Added Cutscene
    Opening Scene added to game

---
+++ ChibiNinjaFinal/Assets/DelayedActivation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DelayedActivation : MonoBehaviour
+{
+    public float delayTime;
+    public GameObject[] objects;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        foreach(GameObject obj in objects)
+        {
+            obj.SetActive(false);
+        }
+    }
+    void Start()
+    {
+        Invoke("Activate", delayTime);
+    }
+    private void Activate()
+    {
+        foreach(GameObject obj in objects)
+        {
+            obj.SetActive(true);
+        }
+    }
+}
---
+++ ChibiNinjaFinal/Assets/TurnOffCinemachineBrain.cs
@@ -0,0 +1,19 @@
+﻿using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOffCinemachineBrain : MonoBehaviour
+{
+    public float timeToTurnOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("TurnOffCinemachineBrainMethod", timeToTurnOff);
+    }
+    private void TurnOffCinemachineBrainMethod()
+    {
+        GetComponent<CinemachineBrain>().enabled = false;        
+        print("BrainOFF");
+    }
+}
